dc85e3964e781a99576b546d40309b75b7a73e24
Fuechschen <me@fuechschen.org>
2016-08-31 22:50:11 +0200

    make this using the db-module

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var request = require('request');
-var Sequelize = require('sequelize');
-var path = require('path');
 var CronJob = require('cron').CronJob;
-var Redis = require('ioredis');
 var storyboard = require('storyboard');
+var DB = require('db');
 
 storyboard.addListener(require('storyboard/lib/listeners/console').default);
 
@@ -11,27 +9,11 @@ var story = storyboard.mainStory;
 
 var config = require('./config');
 
-config.db.options.logging = (toLog)=> {
-    story.debug('SQL', toLog);
-};
-
-var redis = new Redis(config.redis);
-var sub = new Redis(config.redis);
-var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, config.db.options);
-
-var models = {
-    TwitchWatcher: sequelize.import(path.join(__dirname, 'sql_models', 'TwitchWatcher')),
-    TwitchChannel: sequelize.import(path.join(__dirname, 'sql_models', 'TwitchChannel'))
-};
-
-models.TwitchWatcher.belongsTo(models.TwitchChannel);
-models.TwitchChannel.hasMany(models.TwitchWatcher);
-
-sequelize.sync();
+var db = new DB(config.db);
 
 var cron = new CronJob('0 */5 * * * *', function () {
     story.info('Starting check.');
-    models.TwitchChannel.findAll().then((channels)=> {
+   db.models.TwitchChannel.findAll().then((channels)=> {
         channels.forEach((channel)=> {
             request.get(channel.api_url, {headers: {'Client-ID': config.api.twitch}}, (err, resp, body)=> {
                 if (!err && [200, 304].indexOf(resp.statusCode) !== -1) {
@@ -42,19 +24,15 @@ var cron = new CronJob('0 */5 * * * *', function () {
                     } else if (!channel.status && body.stream !== null && channel.stream_id !== body.stream._id) {
                         channel.update({status: true, stream_id: body.stream._id}).then(()=> {
                             channel.getTwitchWatchers().then((watchers)=> {
-                                redis.publish(config.redis.pubsub.prefix + 'events', JSON.stringify({
-                                    type: 'twitchAnnounce',
-                                    sid: '11111111',
-                                    data: {
-                                        channels: watchers.map((watcher)=> {
-                                            return watcher.server_channel
-                                        }),
-                                        ch_name: body.stream.channel.display_name,
-                                        ch_link: body.stream.channel.url,
-                                        str_title: body.stream.channel.status,
-                                        str_game: body.stream.game
-                                    }
-                                }));
+                                db.sendEvent('twitchAnnounce',{
+                                    channels: watchers.map((watcher)=> {
+                                        return watcher.server_channel
+                                    }),
+                                    ch_name: body.stream.channel.display_name,
+                                    ch_link: body.stream.channel.url,
+                                    str_title: body.stream.channel.status,
+                                    str_game: body.stream.game
+                                });
                                 story.info('Channel ' + channel.channel + ' is now online.')
                             });
                         });
@@ -64,10 +42,5 @@ var cron = new CronJob('0 */5 * * * *', function () {
         })
     });
 }, null, true);
-
-sub.subscribe(config.redis.pubsub.prefix + 'events');
-sub.on('message', (channel, message)=> {
-    story.info(channel, message);
-});
 
 story.info('Watcher started.');
